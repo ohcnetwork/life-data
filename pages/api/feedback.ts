7d15639a644823f1ddb8dace12f75935ae5c0b8b
divyagar <divyagarg2601@gmail.com>
2021-05-05 16:17:47 +0530

    Add captcha verification
    Co-authored-by: Karan Sanjeev <yo@alphaman.me>
    Co-authored-by: Arpan Abhishek <arpanforbusiness@gmail.com>

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import * as AWS from 'aws-sdk'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
@@ -8,37 +9,55 @@ AWS.config.update({
 })
 var sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
 
+const isCaptchaVerified = async (captchaResponse: string) => {
+    const { data } = await axios.post('https://www.google.com/recaptcha/api/siteverify', [], {
+        params: {
+            secret: process.env.RECAPTCHA_SECRET,
+            response: captchaResponse
+        }
+    })
+
+    return data.success
+}
+
 const handler = async (req: VercelRequest, res: VercelResponse) => {
     const { feedback, external_id } = req.body
+    const captchaRes = req.body['g-recaptcha-response']
     const sqsParams: AWS.SQS.SendMessageRequest = {
         QueueUrl: process.env.AWS_SQS_URL,
         MessageBody: 'Feedback submitted by user, ' + feedback,
         MessageAttributes: {
-            "Feedback": {
-                DataType: "String",
+            'Feedback': {
+                DataType: 'String',
                 StringValue: feedback
             },
-            "External ID": {
-                DataType: "String",
+            'External ID': {
+                DataType: 'String',
                 StringValue: external_id
-            }
+            },
         }
     }
 
-   return sqs.sendMessage(sqsParams, (err, data) => {
-        if (!err) {
-            return res.json({
-                type: 'success',
-                message: 'Feedback has been successfully recorded'
-            })
-        }
+    if (await isCaptchaVerified(captchaRes)) {
+        return sqs.sendMessage(sqsParams, (err, data) => {
+            if (!err) {
+                return res.json({
+                    type: 'success',
+                    message: 'Feedback has been successfully recorded'
+                })
+            }
 
-        return res.json({
-            type: 'error',
-            message: 'Error while recording feedback',
-            data: err.message
-        }).status(500)
-    })
+            return res.json({
+                type: 'error',
+                message: 'Error while recording feedback'
+            }).status(500)
+        })
+    }
+
+    return res.json({
+        type: 'error',
+        message: 'Captcha not verified'
+    }).status(500)
 }
 
 export default handler
