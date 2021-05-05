ac62f60985cca5f126138ccb01cfe11855a3be37
Karan Sanjeev <yo@alphaman.me>
2021-05-05 15:22:10 +0530

    Basic feedback recording done
    Co-authored-by: Arpan Abhishek <arpanforbusiness@gmail.com>
    Co-authored-by: Divya Garg <divyagarg2601@gmail.com>

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,7 +1,40 @@
+import * as AWS from 'aws-sdk'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
-const handler = (req: VercelRequest, res: VercelResponse) => {
-    res.json({ hello: 'world' })
+AWS.config.update({
+    accessKeyId: process.env.AWS_SQS_ACCESS,
+    secretAccessKey: process.env.AWS_SQS_SECRET,
+    region: process.env.AWS_SQS_REGION
+})
+var sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+    const { feedback, external_id } = req.body
+    const sqsParams: AWS.SQS.SendMessageRequest = {
+        QueueUrl: process.env.AWS_SQS_URL,
+        MessageBody: 'Feedback submitted by user, ' + feedback,
+        MessageAttributes: {
+            "Feedback": {
+                DataType: "String",
+                StringValue: feedback
+            }
+        }
+    }
+
+   return sqs.sendMessage(sqsParams, (err, data) => {
+        if (!err) {
+            return res.json({
+                type: 'success',
+                message: 'Feedback has been successfully recorded'
+            })
+        }
+
+        return res.json({
+            type: 'error',
+            message: 'Error while recording feedback',
+            data: err.message
+        }).status(500)
+    })
 }
 
 export default handler
