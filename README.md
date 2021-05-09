4b88f694568c9c6f769923a40a99d4adcd60028b
khavinshankar <khavinshankar@gmail.com>
2021-05-09 11:12:57 +0530

    added optional parameter epoch

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 ## Special API
 
 ```
-endpoint: https://life-pipeline.coronasafe.network/api/resources?state=state_name&district&district_name&resource=resource_type
+endpoint: https://life-pipeline.coronasafe.network/api/resources?state=state_name&district&district_name&resource=resource_type&epoch=number
 params:
   - state: state_name
   - district: district_name
   - resource: oxygen | medicine | hospital | helpline | ambulance | food
+  - (optional) epoch: number (verified in last n days)
 ```
 
 ### Example
--- pages/api/resources.js
+++ pages/api/resources.js
@@ -9,7 +9,7 @@ const handler = async (req, res) => {
      optionsSuccessStatus: 200,
   });
 
-  let { state, district, resource } = req.query;
+  let { state, district, resource, epoch } = req.query;
 
   resource = parametreize(resource)
 
@@ -21,7 +21,7 @@ const handler = async (req, res) => {
       case "helpline":
       case "hospital":
       case "food":
-        res.status(200).json({ data: findResource(state, district, resource) });
+        res.status(200).json({ data: findResource(state, district, resource).filter(withEpoch(epoch)) });
         break;
       default:
         res
@@ -32,6 +32,17 @@ const handler = async (req, res) => {
     res
       .status(405)
       .json({ error: "Please make sure that the state, district and resource are present and valid" });
+  }
+}
+
+const withEpoch = (epoch) => {
+  return record => {
+    const verified_date = new Date(record.last_verified_on);
+    const epoch_date = new Date();
+    epoch_date.setDate(epoch_date.getDate() - epoch);
+  
+    console.log(epoch, epoch_date)
+    return !(epoch_date >= verified_date);
   }
 }
 
